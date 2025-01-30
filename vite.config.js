0f2344fa4ba7d8b3bb63fef1fd4ec8da458b5a38
unknown <paola.lemorvan@gmail.com>
2025-01-30 12:57:09 +0100

    forth deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/valme/', // Remplace "ton-repo" par le nom de ton dépôt GitHub
+
 })
